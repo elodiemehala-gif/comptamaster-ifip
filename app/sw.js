6b7bb47bb24a8b25df56fb59222335420bb6927d
elodiemehala-gif <elodiemehala@gmail.com>
2026-07-22 15:48:39 +0200

    Actualiser le cache hors ligne

--- app/sw.js
+++ app/sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'comptamaster-v1';
+const CACHE_NAME = 'comptamaster-v2';
 const APP_SHELL = [
   './',
   './index.html',
   './styles.css',
   './app.js',
   './data.js',
+  './formula-details.js',
   './manifest.webmanifest',
   './icons/icon-192.png',
   './icons/icon-512.png',
